5ad0bcc6928ca7ed2b63778d1614f16bc24da36e
Alyssa <77090918+jonesal5@users.noreply.github.com>
2021-06-08 10:18:35 -0400

    Add files via upload

---
+++ playingwithobjects.js
@@ -0,0 +1,32 @@
+// How would you print/log John's age?
+console.log(John[1].age){
+}
+
+// How would you print/log the name of the first object?
+console.log(){User[0].name
+}
+
+// How would you print/log the name and age of each user using a for loop?
+console.log(){
+    var users = [
+        {
+          first name: Michael,
+          age: 37,
+          },
+        {
+          first name: John,
+          age: 30,
+           },
+        {
+            first name: David,
+            age: 27,
+        }
+      ];
+}
+for (var i = 0; i < users.length; i++){
+    console.log(Michael[i].name, "-", Michael[i].age);
+    (var i = 1; i < users.length; i++){
+        console.log(John[i].name, "-", John[i].age);
+    (var i = 2; i < users.length; i++){
+            console.log(David[i].name, "-", David[i].age);
+}
